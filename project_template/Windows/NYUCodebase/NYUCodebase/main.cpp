ef68d495842db6f563dd7d25069b0fed3dd155ad
Sean Whalen <seanwhalen42@gmail.com>
2014-09-14 16:19:09 -0400

    added prefab functions for loading images and drawing sprites

--- project_template/Windows/NYUCodebase/NYUCodebase/main.cpp
+++ project_template/Windows/NYUCodebase/NYUCodebase/main.cpp
@@ -2,8 +2,21 @@
 #include <SDL.h>
 #include <SDL_opengl.h>
 #include <SDL_image.h>
+#include <string>
 
 SDL_Window* displayWindow;
+
+GLuint LoadTexture(const char *image_path) {
+	SDL_Surface *surface = IMG_Load(image_path);
+	GLuint textureID;
+	glGenTextures(1, &textureID);
+	glBindTexture(GL_TEXTURE_2D, textureID);
+	glTexImage2D(GL_TEXTURE_2D, 0, 4, surface->w, surface->h, 0, GL_RGBA, GL_UNSIGNED_BYTE, surface->pixels);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	SDL_FreeSurface(surface);
+	return textureID;
+}
 
 void DrawSprite(GLint texture, float x, float y, float rotation) {
 	glEnable(GL_TEXTURE_2D);
@@ -31,18 +44,26 @@ int main(int argc, char *argv[])
 	SDL_GLContext context = SDL_GL_CreateContext(displayWindow);
 	SDL_GL_MakeCurrent(displayWindow, context);
 
+
 	bool done = false;
-	
+	float lastFrameTicks = 0.0f;
+	GLfloat triangle[] = { 0.0f, 0.5f, -0.5f, -0.5f, 0.5f, -0.5f };
+	std::string imagePathStr = "assets\graphics\Boardgame pack(520 assets)\PNG\Cards\cardSpadesA.png";
+	const char* image_path = imagePathStr.c_str();
+	GLuint aceOfSpades = LoadTexture(image_path);
+	DrawSprite(aceOfSpades, 0.0f, 0.0f, 0.0f);
+	glVertexPointer(2, GL_FLOAT, 0, triangle);
+	glEnableClientState(GL_VERTEX_ARRAY);
+	//glTranslatef(1.0, 0.0, 0.0);
+	glEnableClientState(GL_COLOR_ARRAY);
+	GLfloat triangleColors[] = { 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0 };
+	glColorPointer(3, GL_FLOAT, 0, triangleColors);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
 	SDL_Event event;
 	while (!done) {
-		GLfloat triangle[] = { 0.0f, 0.5f, -0.5f, -0.5f, 0.5f, -0.5f };
-		glVertexPointer(2, GL_FLOAT, 0, triangle);
-		glEnableClientState(GL_VERTEX_ARRAY);
-		glTranslatef(1.0, 0.0, 0.0);
-		glEnableClientState(GL_COLOR_ARRAY);
-		GLfloat triangleColors[] = { 1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0 };
-		glColorPointer(3, GL_FLOAT, 0, triangleColors);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		float ticks = (float)SDL_GetTicks() / 1000.0f;
+		float elapsed = ticks - lastFrameTicks;
+		lastFrameTicks = ticks;
 		while (SDL_PollEvent(&event)) {
 			if (event.type == SDL_QUIT || event.type == SDL_WINDOWEVENT_CLOSE) {
 				done = true;
