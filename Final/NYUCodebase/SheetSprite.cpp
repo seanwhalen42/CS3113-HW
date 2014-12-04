eaa437db5b06c53c2f36f1128c851a081fdcf4ff
unknown <seanwhalen42@gmail.com>
2014-12-03 19:16:41 -0500

    Began work on SheetSprite class

---
+++ Final/NYUCodebase/SheetSprite.cpp
@@ -0,0 +1,42 @@
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include <SDL_image.h>
+#include "SheetSprite.h"
+
+SheetSprite::SheetSprite(){}
+
+SheetSprite::SheetSprite(GLuint texture, float sheetX, float sheetY, float pixelU, float pixelV, float pixelHeight, float pixelWidth) : texture(texture),
+sheetX(sheetX), sheetY(sheetY){
+	aspectRatio = sheetX / sheetY;
+	u = pixelU / sheetX;
+	v = pixelV / sheetY;
+	height = pixelHeight / sheetY;
+	width = pixelWidth / sheetX;
+}
+
+float SheetSprite::getHeight(){
+	return height;
+}
+
+float SheetSprite::getWidth(){
+	return width;
+}
+
+void SheetSprite::draw(float x, float y, float scale){
+	GLfloat quad[] = { -width / 2 * scale * aspectRatio, height / 2 * scale, -width / 2 * scale * aspectRatio, -height / 2 * scale,
+		width / 2 * scale * aspectRatio, -height / 2 * scale, width / 2 * scale * aspectRatio, height / 2 * scale };
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glTranslatef(x, y, 0);
+
+	GLfloat quadUVs[] = { u, v, u, v + height, u + width, v + height, u + width, v };
+	glVertexPointer(2, GL_FLOAT, 0, quad);
+	glTexCoordPointer(2, GL_FLOAT, 0, quadUVs);
+	glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+
+	glDrawArrays(GL_QUADS, 0, 4);
+	glDisable(GL_TEXTURE_2D);
+}
---
+++ Final/NYUCodebase/SheetSprite.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include <SDL_image.h>
+#include "Config.h"
+
+class SheetSprite{
+public:
+	SheetSprite();
+	SheetSprite(GLuint texture, float sheetX, float sheetY, float pixelU, float pixelV, float pixelHeight, float pixelWidth);
+	~SheetSprite();
+
+	//Accessors
+	float getHeight();
+	float getWidth();
+	void draw(float x, float y, float scale);
+private:
+	GLuint texture;
+	float u;
+	float v;
+	float height;
+	float width;
+	float sheetX;
+	float sheetY;
+	float aspectRatio;
+};
